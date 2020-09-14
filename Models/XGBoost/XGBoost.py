d7b6cdbf2cd472b85b794daba99b33871efd670c
zibrahim <zina.ibrahim@gmail.com>
2020-09-14 14:16:53 +0100

    smote works, lstm is bad, but xgboost is good with metrics

--- Models/XGBoost/XGBoost.py
+++ Models/XGBoost/XGBoost.py
@@ -3,6 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.metrics import roc_curve, auc
 import xgboost as xgb
+from sklearn.metrics import roc_auc_score, f1_score, precision_score, recall_score, accuracy_score, classification_report
+
 
 from Utils.Model import stratified_group_k_fold, get_distribution
 def run_xgboost_classifier(X,y, label, groups, experiment_number):
@@ -24,6 +26,7 @@ def run_xgboost_classifier(X,y, label, groups, experiment_number):
     mean_fpr = np.linspace(0, 1, 10) #CROSS VALIDATION CHANGE
     plt.figure(figsize=(10, 10))
 
+    stats_df = pd.DataFrame()
     for fold_ind, (training_ind, testing_ind) in enumerate(stratified_group_k_fold(X, y, groups, k=10)) : #CROSS-VALIDATION
         training_groups, testing_groups = groups[training_ind], groups[testing_ind]
         training_y, testing_y = y[training_ind], y[testing_ind]
@@ -37,11 +40,43 @@ def run_xgboost_classifier(X,y, label, groups, experiment_number):
         xgbm.fit(training_X, training_y)
         y_pred_rt = xgbm.predict_proba(testing_X)[:, 1]
 
+        y_pred_binary = (y_pred_rt > 0.5).astype('int32')
+        F1Macro = f1_score(testing_y, y_pred_binary, average='macro')
+        F1Micro = f1_score(testing_y, y_pred_binary, average='micro')
+        F1Weighted = f1_score(testing_y, y_pred_binary, average='weighted')
+        PrecisionMacro =  precision_score(testing_y, y_pred_binary, average='macro')
+        PrecisionMicro =  precision_score(testing_y, y_pred_binary, average='micro')
+        PrecisionWeighted =  precision_score(testing_y, y_pred_binary, average='weighted')
+        RecallMacro =  recall_score(testing_y, y_pred_binary, average='macro')
+        RecallMicro =  recall_score(testing_y, y_pred_binary, average='micro')
+        RecallWeighted =  recall_score(testing_y, y_pred_binary, average='weighted')
+        Accuracy = accuracy_score(testing_y, y_pred_binary)
+        ClassificationReport = classification_report(testing_y, y_pred_binary)
+
         fpr, tpr, thresholds = roc_curve(testing_y, y_pred_rt)
         tprs.append(np.interp(mean_fpr, fpr, tpr))
         tprs[-1][0] = 0.0
         roc_auc = auc(fpr, tpr)
         aucs.append(roc_auc)
+
+        performance_row = {
+            "F1-Macro" : F1Macro,
+            "F1-Micro" :F1Micro,
+            "F1-Weighted" : F1Weighted,
+            "Precision-Macro" : PrecisionMacro,
+            "Precision-Micro" : PrecisionMicro,
+            "Precision-Weighted": PrecisionWeighted,
+            "Recall-Macro" : RecallMacro,
+            "Recall-Micro": RecallMicro,
+            "Recall-Weighted": RecallWeighted,
+            "Accuracy" : Accuracy,
+            "AUC": auc,
+            "ClassificationReport": ClassificationReport
+        }
+
+        stats_df = stats_df.append(performance_row, ignore_index=True)
+
+
         plt.plot(fpr, tpr, lw=1, alpha=0.3,
                  label='ROC fold %d (AUC = %0.2f)' % (fold_ind, roc_auc))
 
@@ -77,10 +112,12 @@ def run_xgboost_classifier(X,y, label, groups, experiment_number):
     plt.title('XGBoost Cross-Validation ROC', fontsize=18)
     # plt.legend(loc="lower right", prop={'size' : 15})
 
-    stats_path = "Run/Stats"
+    stats_path = "Run/Stats/"
     prediction_path = "Run/Prediction/"
 
     plt.savefig(prediction_path + "Experiment"+ experiment_number+ label + "ROC_XGBoost.pdf")
+
+    stats_df.to_csv(stats_path + label+"XGBoost.csv", index=False)
 
     distr_df = pd.DataFrame(distrs, index=index, columns=[f'Label {l}' for l in range(np.max(y) + 1)])
     distr_df.to_csv(stats_path + "Experiment"+experiment_number+"-K-Fold-Distributions.csv", index=True)
